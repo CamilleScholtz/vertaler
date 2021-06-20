87d35425bf1458a1abced2a5f2c38d9d7482980b
Camille Scholtz <admin@camille.sh>
2021-06-20 18:17:01 +0200

    Initial Commit

---
+++ Vertaler/VertalerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VertalerApp.swift
+//  Vertaler
+//
+//  Created by Camille Scholtz on 20/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct VertalerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
